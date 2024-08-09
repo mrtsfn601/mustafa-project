c2d5f637a1f35535fa702f130ad198cbd9aca68a
Marat Safin <80962941+mrtsfn601@users.noreply.github.com>
2024-08-09 13:36:27 -0400

    update discounted_cash_flow

--- code/utils/intrinsic_value.py
+++ code/utils/intrinsic_value.py
@@ -1,33 +1,47 @@
+import yfinance as yf
+from finvizfinance.quote import finvizfinance
 from prettytable import PrettyTable
 
 
-def discounted_cash_flow(ticker):
+def discounted_cash_flow(ticker_symbol):
     """
-    Calculate the intrinsic value of a business entity using the Discounted Cash Flow (DCF) method.
-    :param ticker: yfinance.Ticker object
+    Calculate the intrinsic value of a business using the Discounted Cash Flow (DCF) method.
     """
-    # todo get TTM operating cash flow value
-    operating_cash_flow = ticker.cashflow.loc['Operating Cash Flow'].iloc[0] / 1_000_000
+    yf_ticker = yf.Ticker(ticker_symbol)
+    fv_ticker = finvizfinance(ticker_symbol)
 
-    short_term_debt = ticker.quarterly_balance_sheet.loc['Current Debt'].iloc[0] / 1_000_000 # in millions
-    long_term_debt = ticker.quarterly_balance_sheet.loc['Long Term Debt'].iloc[0] / 1_000_000 # in millions
+    operating_cash_flow_ttm = sum(
+        yf_ticker.quarterly_cashflow.loc['Operating Cash Flow'].iloc[i] for i in range(4)) / 1_000_000
+
+    short_term_debt = yf_ticker.quarterly_balance_sheet.loc['Current Debt'].iloc[0] / 1_000_000
+    long_term_debt = yf_ticker.quarterly_balance_sheet.loc['Long Term Debt'].iloc[0] / 1_000_000
     total_debt = short_term_debt + long_term_debt
 
+    cash = yf_ticker.quarterly_balance_sheet.loc['Cash Cash Equivalents And Short Term Investments'].iloc[0] / 1_000_000
+
+    print(fv_ticker.ticker_fundament())
+    cash_flow_1_5y = float(fv_ticker.ticker_fundament()['EPS next 5Y'].strip('%'))
+    cash_flow_6_10y = min(cash_flow_1_5y, 15.00)
+    cash_flow_10_20y = min(cash_flow_6_10y, 4.00)
+
+    shares_outstanding_str = fv_ticker.ticker_fundament()['Shs Outstand']  # 1.65B or 1.65M
+    shares_outstanding = float(shares_outstanding_str[:-1])  # in millions
+    if shares_outstanding_str[-1] == 'B':  # in billions
+        shares_outstanding *= 1_000
+
     table = PrettyTable()
-    table.field_names = ["Metric", "Value"]
-    table.add_row(["Operating Cash Flow (Annual TTM, $ in millions)", operating_cash_flow])
-    table.add_row(["Short Term Debt (Quarterly, $ in millions)", short_term_debt])
-    table.add_row(["Long Term Debt (Quarterly, $ in millions)", long_term_debt])
-    table.add_row(["Total Debt (Quarterly, $ in millions)", total_debt])
+    table.field_names = ["Metric", "Unit", "Value"]
+    table.add_row(["Operating Cash Flow (TTM)", "$M", operating_cash_flow_ttm])
+    table.add_row(["Short Term Debt (Quarter)", "$M", short_term_debt])
+    table.add_row(["Long Term Debt (Quarter)", "$M", long_term_debt])
+    table.add_row(["Total Debt (Quarter)", "$M", total_debt])
+    table.add_row(["Cash, Cash Equivalents & Short Term Investments (Quarter)", "$M", cash])
+    table.add_row(["Cash Flow Growth Rate   1-5 Years", "%", cash_flow_1_5y])
+    table.add_row(["Cash Flow Growth Rate  6-10 Years", "%", cash_flow_6_10y])
+    table.add_row(["Cash Flow Growth Rate 10-20 Years", "%", cash_flow_10_20y])
+    table.add_row(["Shares Outstanding", "M", shares_outstanding])
     table.align["Metric"] = "l"
     table.align["Value"] = "r"
     print(table)
 
-    # cash_flow = ticker.cashflow.loc['Total Cash From Operating Activities'].iloc[0]
-    # growth_rate = get_growth_rate(ticker, years)
-    # discount_rate = get_discount_rate(ticker)
-    # dcf = sum([cash_flow * (1 + growth_rate) ** year / (1 + discount_rate) ** year for year in range(1, years + 1)])
-    # shares_outstanding = ticker.info['sharesOutstanding']
-    # intrinsic_value = dcf / shares_outstanding
-    # return intrinsic_value
-    return 1
+    return table
